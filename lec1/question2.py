ab3a246fd6741e3ff89785a5898adac4aece7e6f
nac-39 <83295760+nac-39@users.noreply.github.com>
2023-05-18 12:32:11 +0900

    lec1: question2: 辞書同士の比較にした

--- lec1/question2.py
+++ lec1/question2.py
@@ -1,5 +1,15 @@
 from pathlib import Path
 import time
+from collections import Counter
+
+
+def save_answer_file(file_name, answers):
+    answer_file = Path(__file__).parent / \
+        Path("anagrams/") / Path(file_name + "_answer.txt")
+    with open(answer_file, "w") as f:
+        for r in answers:
+            f.write(str(r) + "\n")
+
 
 def check_score(word):
     SCORES = [1, 3, 2, 2, 1, 3, 3, 1, 1, 4, 4, 2,
@@ -31,68 +41,88 @@ def get_data(file_name):
 def search_word(target, word_list):
     answers = []
     for word in word_list:
-        if all([target[i] >= word[0][i] for i in range(26)]):
+        flag = True
+        for key, value in word[0].items():
+            if not (key in target) or target[key] < value:
+                flag = False
+        if flag:
             answers.append(word[1])
     return answers
 
 
-def get_letter_count(word):
-    """文字列wordを受け取り、アルファベットがそれぞれ何個入っているか返す
+def get_counted_dictionary(dictionary):
+    """辞書ファイルを受け取り、文字数を数えて返す
 
     Args:
-        word (str): 文字列（アルファベットのみ）
+        dictionary (list[str]): 辞書ファイルの中身
+
+    Returns:
+        dict: 文字数が16文字以下の辞書と、全部入りの辞書
 
     Tests:
-    >>> get_letter_count('a')
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    >>> get_letter_count('z')
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+    >>> get_counted_dictionary(['a', 'ab', 'abcc', 'aaaaaaaaaaaaaabb'])
+    {'small': [(Counter({'a': 1}), 'a'), (Counter({'a': 1, 'b': 1}), 'ab'), (Counter({'c': 2, 'a': 1, 'b': 1}), 'abcc'), (Counter({'a': 14, 'b': 2}), 'aaaaaaaaaaaaaabb')], 'normal': [(Counter({'a': 1}), 'a'), (Counter({'a': 1, 'b': 1}), 'ab'), (Counter({'c': 2, 'a': 1, 'b': 1}), 'abcc'), (Counter({'a': 14, 'b': 2}), 'aaaaaaaaaaaaaabb')]}
     """
-    alphabet = [0] * 26
-    for w in word:
-        if ord(w) < 97 or 122 < ord(w):
-            continue
-        alphabet[ord(w) - 97] += 1  # a->0, b->1...z->25になる
-    return alphabet
-
-
-def get_sorted_dictionary(dictionary):
     new_dictionary = []
     for word in dictionary:
         new_dictionary.append(
-            (get_letter_count(word), word)
+            (Counter(word), word)
         )  # (文字カウント済み, 元の文字)
-    new_dictionary = sorted(new_dictionary, key=lambda x: x[0])
-    return new_dictionary
+    small_dictionary = [d for d in new_dictionary if len(d[1]) <= 16]
+    return {
+        "small": small_dictionary,  # 188/512に削減
+        "normal": new_dictionary
+    }
 
 
 def search_anagram(word, dictionary):
-    new_dictionary = get_sorted_dictionary(dictionary)
-    sorted_word = get_letter_count(word)
-    anagrams = search_word(sorted_word, new_dictionary)
+    """辞書からwordの一部を使ったアナグラムを探す
+
+    Args:
+        word (str): 探したい文字列  
+        dictionary list[(Tuple(Counter, str))]: 辞書
+
+    Returns:
+        list[str]: wordの一部を使ったアナグラム全て
+    
+    Tests:
+    >>> search_anagram('aahlpooo', [(Counter('alpha'), 'alpha')])
+    ['alpha']
+    >>> search_anagram('', [(Counter('alpha'), 'alpha')])
+    []
+    """
+    counted_word = Counter(word)
+    anagrams = search_word(counted_word, dictionary)
     return anagrams
 
 
 def main():
     """与えられた文字列の一部を使ったAnagramを辞書ファイルから探して全て返す"""
-    res = []
-    data_files = ["small"]
-    count = 0
-    dictionary = get_dictionary()
+    data_files = ["small", "medium"]
+    _dictionary = get_dictionary()
     for data_file in data_files:
+        res = []
+        if data_file == "small" or data_file == "medium":
+            dictionary = get_counted_dictionary(_dictionary)["small"]
+        else:
+            dictionary = get_counted_dictionary(_dictionary)["normal"]
+        data = get_data(data_file + ".txt")
+
+        # 探索
         start_time = time.perf_counter()
-        for data in get_data(data_file + ".txt"):
-            anagrams = search_anagram(data, dictionary)
+        for i, d in enumerate(data):
+            anagrams = search_anagram(d, dictionary)
             ans = max(anagrams, key=check_score, default=[])
-            print(count, ans)
+            if i % 100 == 0:
+                print(i, ans)
             res.append(ans)
-            count += 1
         end_time = time.perf_counter()
-        answer_file = Path(__file__).parent /  Path("anagrams/") / Path(data_file + "_answer.txt")
-        with open(answer_file, "w") as f:
-            for r in res:
-                f.write(str(r) + "\n")
-            print("time: " + str(end_time - start_time) + "s")
+        
+        # 結果を出力
+        save_answer_file(data_file, res)
+        with open("time.txt", "a") as f:
+            f.write(data_file + " time: " + str(end_time - start_time) + "s\n")
+        print(data_file + " time: " + str(end_time - start_time) + "s")
 
 
 if __name__ == "__main__":
--- lec1/result.md
+++ lec1/result.md
@@ -1,17 +1,12 @@
 # 実行結果
 
-1.
+1. score_checker ではなく max で比較していたバージョン
 
-```python
-% python score_checker.py small.txt ../../small.txt_answer.txt
 You answer is correct! Your score is 193.
-% python score_checker.py medium.txt ../../medium_answer.txt
 You answer is correct! Your score is 18643.
-% python score_checker.py large.txt ../../large_answer.txt
 You answer is correct! Your score is 221999.
-```
 
-2.
+2. score_checker を使うようになったバージョン
 
 small:
 time: 5.045994250001968s
@@ -20,6 +15,20 @@ medium:
 time: 323.4841967919929s
 You answer is correct! Your score is 18911.
 
-large: 
+large:
 time: 3721.087472542s
 You answer is correct! Your score is 244642.
+
+3. dict を使うようにしたバージョン
+
+small:
+time: 2.695723374999943s
+You answer is correct! Your score is 193.
+
+medium:
+You answer is correct! Your score is 18911.
+time: 287.2076153750095s
+
+large:
+The number of words in large.txt and large_answer.txt doesn't match.
+time: 3146.746209332996s
