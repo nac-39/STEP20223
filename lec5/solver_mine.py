0496db1fe0b98ab3313e9db8cbd77e7cdd558758
nac-39 <83295760+nac-39@users.noreply.github.com>
2023-06-15 15:31:12 +0900

    lec5: wip

--- lec5/solver_mine.py
+++ lec5/solver_mine.py
@@ -1,17 +1,49 @@
 #!/usr/bin/env python3
 
 import sys
+import math
+import random
 
 from common import print_tour, read_input
 
+INF = 10**9
+
+
+def distance(city1, city2):
+    return math.sqrt((city1[0] - city2[0]) ** 2 + (city1[1] - city2[1]) ** 2)
+
+
+class Tour:
+    def __init__(self, cities: list[tuple[int, int]]) -> None:
+        self.tour = random.sample(range(len(cities)), len(cities))
+        self.cities = cities
+
+    def get_score(self):
+        """ルートのスコアを計算する
+
+        Args:
+            cities (list[tuple[int, int]]): 都市の座標の一覧
+            tour (list[int]): 道順
+
+        Returns:
+            int: tourの順番に年を訪れた時のスコア
+        """
+        current_city = self.tour[0]
+        next_city = self.tour[1]
+        score_sum = 0
+        for city in self.tour:
+            score = distance(self.cities[current_city], self.cities[next_city])
+            score_sum += score
+            next_city = current_city
+            current_city = city
+        return score_sum
+
 
 def solve(cities):
-    # Build a trivial solution.
-    # Visit the cities in the order they appear in the input.
-    return list(range(len(cities)))
+    N = len(cities)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     assert len(sys.argv) > 1
     tour = solve(read_input(sys.argv[1]))
     print_tour(tour)
